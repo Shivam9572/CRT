c1f5d5fe7e619214a95b081eacf480b6d32eb3d0
Shivam9572 <shivamroy9572@gamil.com>
2025-09-09 21:36:26 +0530

    clean code task upload

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,7 @@
 const http=require('http');
-const fs=require('fs');
-const { error } = require('console');
-
-const server=http.createServer((req,res)=>{
-    
-    res.setHeader('Content-Type','text/html');
-    if(req.url=="/"){
-        res.statusCode=302;
-        res.end(`<form method='post' action='/message'>
-               <label id='username'>username:</label>
-               <input type='text' name='username' id='username'>
-               <button type='submit'>Add</button>
-               </button>`);
-    }else{
-        if(req.url=='/message'){
-            res.setHeader('Content-Type','text/html');
-             let dataChunks=[];
-             req.on('data',(chunks)=>{
-               dataChunks.push(chunks);
-             });
-             req.on('end',()=>{
-                fs.writeFileSync("file.text",dataChunks.toString().split('=')[1]);
-                res.end("add data in file");
-             });
-
-        }
-        else if(req.url=='/read'){
-            res.end(`<h1>${fs.readFileSync('file.text',(error,data)=>{
-                if(error){
-                    res.end("error");
-                }
-                res.end(`<h1>${data}</html>`);
-            })}`)
-        }
-    }
-    
-})
+const routesHandler=require('./routes.js');
+console.log(routesHandler.testFunction);
+const server=http.createServer(routesHandler.routes)
 server.listen(3000,()=>{
    console.log("port 3000 is listen");
 })
