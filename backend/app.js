b489f20fa998684c5ccaffb4928304cc9d362d91
Shivam9572 <shivamroy9572@gamil.com>
2025-09-09 17:24:56 +0530

    write and read file from buffer task upload

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,40 @@
 const http=require('http');
+const fs=require('fs');
+const { error } = require('console');
 
 const server=http.createServer((req,res)=>{
+    
     res.setHeader('Content-Type','text/html');
-    if(req.url=='/'){
-        res.statusCode=200;
-        res.end("<h1>Hello world</h1>");
-    }else if(req.url=='/pizza'){
-        res.statusCode=200;
-        res.end("<h1>This is your pizza</h1>")
-    }else if(req.url=='/home'){
-        res.statusCode=200;
-        res.end("<h1>Welcome Home</h1>")
-    }else if(req.url=='/about'){
-        res.statusCode=200;
-        res.end("<h1>Welcome to about us</h1>")
-    }else if(req.url=='/node'){
-        res.statusCode=200;
-        res.end("<h1>Welcome to my node js project</h1>");
+    if(req.url=="/"){
+        res.statusCode=302;
+        res.end(`<form method='post' action='/message'>
+               <label id='username'>username:</label>
+               <input type='text' name='username' id='username'>
+               <button type='submit'>Add</button>
+               </button>`);
     }else{
-        res.statusCode=404;
-        res.end("<h1>page not found </h1>");
+        if(req.url=='/message'){
+            res.setHeader('Content-Type','text/html');
+             let dataChunks=[];
+             req.on('data',(chunks)=>{
+               dataChunks.push(chunks);
+             });
+             req.on('end',()=>{
+                fs.writeFileSync("file.text",dataChunks.toString().split('=')[1]);
+                res.end("add data in file");
+             });
+
+        }
+        else if(req.url=='/read'){
+            res.end(`<h1>${fs.readFileSync('file.text',(error,data)=>{
+                if(error){
+                    res.end("error");
+                }
+                res.end(`<h1>${data}</html>`);
+            })}`)
+        }
     }
+    
 })
 server.listen(3000,()=>{
    console.log("port 3000 is listen");
