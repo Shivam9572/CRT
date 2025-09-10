afedbeed4761963f292010c7df66cca0644d4692
Shivam9572 <shivamroy9572@gamil.com>
2025-09-10 21:13:38 +0530

    upload create middleware task

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 const express=require("express");
  const app=express();
-
-
+ function midddleWare(req,res,next){
+  req.user="Guest";
+  next();
+ }
+app.get("/welcome",midddleWare,(req,res)=>{
+   res.setHeader("content-type","text/html");
+   res.send(`<h1>${req.user}</h1>`);
+})
  app.listen(3000,()=>{
    console.log("Server is up and running on port 3000! Ready to handle requests.");
  })
