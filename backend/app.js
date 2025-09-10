a7fecc9caab811f4733d73ff4e8e413ba4f641db
Shivam9572 <shivamroy9572@gamil.com>
2025-09-10 21:59:43 +0530

    routes task upload

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express=require("express");
  const app=express();
- function midddleWare(req,res,next){
-  req.user="Guest";
-  next();
- }
-app.get("/welcome",midddleWare,(req,res)=>{
-   res.setHeader("content-type","text/html");
-   res.send(`<h1>${req.user}</h1>`);
-})
+
+ app.get("/orders",(req,res)=>{
+      res.send("<h1>Here is the list of all orders.</h1>");
+ });
+ app.post("/orders",(req,res)=>{
+    res.send("<h1>A new order has been created.</h1>");
+ });
+ app.get("/users",(req,res)=>{
+   res.send("<h1>Here is the list of all users.</h1>");
+ });
+ app.post("/users",(req,res)=>{
+   res.send("<h1>A new user has been added.</h1>");
+ });
  app.listen(3000,()=>{
-   console.log("Server is up and running on port 3000! Ready to handle requests.");
+  console.log("3000 port is now servering");
  })
