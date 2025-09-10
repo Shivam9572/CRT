db0a191ee7dc12b1df38775db3435867b93c9df5
Shivam9572 <shivamroy9572@gamil.com>
2025-09-10 22:48:09 +0530

    uplod middleware logging task

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,25 @@
-const express = require("express");
-const app = express();
-
-app.get("/welcome/:id", (req, res) => {
-  
-    res.end(`<h1>Welcome ${req.params.id}! you are a ${req.query.role}</h1>`);
-  
-  
-});
-
-
-app.use((req, res) => {
-  res.status(404);
-  res.send("<h1>404 - Page Not Found</h1>");
+const express=require("express");
+ const app=express();
+app.use((req,res,next)=>{
+   console.log(`${req.method} request made to ${req.url}`);
+   next();
 })
-app.listen(4000, () => {
+ app.get("/products",(req,res)=>{
+      res.send("<h1>Here is the list of all products.</h1>");
+ });
+ app.post("/products",(req,res)=>{
+    res.send("<h1>A new product has been added.</h1>");
+ });
+ app.get("/categories",(req,res)=>{
+   res.send("<h1>Here is the list of all categories.</h1>");
+ });
+ app.post("/categories",(req,res)=>{
+   res.send("<h1>A new category has been created.</h1>");
+ });
+ app.use((req,res)=>{
+    res.status(404);
+    res.send("<h1>404 - Page Not Found</h1>");
+ })
+ app.listen(4000,()=>{
   console.log("3000 port is now servering");
-})
+ })
