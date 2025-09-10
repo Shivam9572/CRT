8b2e3fef9b45ae19c71e47a8581f72f07db6a270
Shivam9572 <shivamroy9572@gamil.com>
2025-09-10 17:52:25 +0530

    express setup task upload

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
-const http=require('http');
-const routesHandler=require('./routes.js');
-console.log(routesHandler.testFunction);
-const server=http.createServer(routesHandler.routes)
-server.listen(3000,()=>{
-   console.log("port 3000 is listen");
-})
+const express=require("express");
+ const app=express();
+
+
+ app.listen(3000,()=>{
+   console.log("Server is up and running on port 3000! Ready to handle requests.");
+ })
