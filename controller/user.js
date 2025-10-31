585ad593e8aec8b13a6e7e08627bf027f3b98e07
Shivam9572 <shivamroy9572@gamil.com>
2025-10-31 19:50:07 +0530

    upload code of task

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,5 @@
 const userModel=require("../models/users");
-
+const bcrypt=require("bcrypt");
 module.exports.signup=async(req,res)=>{
     if(!req.body){
         res.send("errr");
@@ -13,15 +13,24 @@ module.exports.signup=async(req,res)=>{
    try {
     let user=await userModel.findByPk(email);
     if(user){
-        res.send({"failed":`${email} already exits`});
+        res.send({failed:`${email} already exits`});
         return;
     }
     if(password!=cpassword){
         res.send({"failed":`password does not match with confirm password`});
         return;
     }
-    let respond=await userModel.create({name:name,email:email,password:password});
-    res.send({"sucess":respond});
+    bcrypt.hash(password,10,async(err,hash)=>{
+        if(!err){
+                let respond=await userModel.create({name:name,email:email,password:hash});
+                  res.send({"sucess":respond});
+        }else{
+            throw new Error("bcrypt error");
+        }
+        
+    })
+    
+  
    } catch (error) {
     console.log(error);
     res.status(404).send({"failed":error.message});
@@ -32,23 +41,29 @@ module.exports.login=async(req,res)=>{
    
    let {email,password}=req.body;
    if(!email ||  !password) {
-    res.send({"failed":"all fields are requiered"});
+    res.status(404).json({success:false,messsage:"all fields are requiered"});
     return;
    }
    try {
     let user=await userModel.findByPk(email);
     if(!user){
-        res.send({"failed":`${email} does not exits`});
+        res.status(400).json({success:false,failed:`${email} does not exits`});
         return;
     }
-    if(user.password != password){
-        res.send({"failed":`please enter correct password`});
-        return;
-    }
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(err){
+            throw new Error("bcrypt error");
+        }
+        if(result){
+              res.send({success:true,message:"user logged in"});
+        }else{
+             res.send({success:false,message:"please enter correct password"});
+        }
+    })
    
-    res.send({"sucess":user});
+   
    } catch (error) {
     console.log(error);
-    res.status(404).send({"failed":error.message});
+    res.status(404).send({success:false,message:error.message});
    }
 }
